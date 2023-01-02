8ba6e19c96a251be2668d48c2276f322acc746a2
hmoon630 <skyuecx0630@gmail.com>
2023-01-02 14:13:37 +0900

    get object with boto3

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import logging
-from fastapi import FastAPI, Response
 from datetime import datetime
+
+from fastapi import FastAPI, Response
+import boto3
 
 logger = logging.getLogger('uvicorn')
 app = FastAPI()
@@ -23,3 +25,20 @@ def now(http_res: Response):
     now = datetime.now()
     logger.info(str(now))
     return str(now)
+
+@app.get("/bucket")
+def bucket(http_res: Response):
+    client = boto3.client('s3')
+    BUCKET = "hmoon-skills.net"
+    KEY = "ACCESS_TEST"
+
+    try:
+        object = client.get_object(Bucket=BUCKET, Key=KEY)
+        body = object["Body"].read().decode('utf-8')
+    except Exception as e:
+        http_res.status_code = 400
+        print(e)
+        return "Could not access the file. Check the permission to access s3 object."
+
+    http_res.status_code = 200
+    return body
